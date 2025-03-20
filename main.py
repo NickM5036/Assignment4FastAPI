b761cd96c4119ae226d72cdfd4daaf4ba17e8366
nick5036 <nicholasmourning579@gmail.com>
2025-03-20 00:07:59 -0400

    feat: added comments for read_item and update_item

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ class Item(BaseModel):
 def read_root():
     return{"Hello": "World"}
 
+#Takes an ID and query for a string and returns it
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] =None):
     return {"item_id": item_id, "q": q}
 
+#Updates an existing item bt its ID
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
